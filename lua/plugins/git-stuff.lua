5cd310e8951a2328274af3bb072279fa1e8abd26
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-01 16:31:09 +0530

    set keybindings for git actions

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,13 +1,18 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>ga", ":Git add -A<CR>", {})
+      vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+      vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+    end,
   },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      vim.keymap.set('n','<leader>gp',":Gitsigns preview_hunk<CR>",{})
-      vim.keymap.set('n','<leader>gt',":Gitsigns toggle_current_line_blame<CR>",{})
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end,
-  }
+  },
 }
