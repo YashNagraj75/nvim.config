2e137f547738e130aa935e13c9dbb911f9ca646e
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-01 17:33:32 +0530

    Add notifier from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,38 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    dashboard = {
-      enabled = true,
-      sections = {
-        { section = "header", padding = 3, position = "relative" },
-        { section = "keys",   gap = 2,     padding = 3,          position = "relative" },
-        { section = "startup" },
-        {
-          section = "terminal",
-          cmd = "ascii-image-converter ~/.config/nvim/yuji-min.jpg -C -c",
-          random = 10,
-          pane = 2,
-          indent = 3,
-          height = 40,
-          padding = 4,
-          position = "relative",
-        },
-      },
-    },
-    lazygit = { configure = false },
-  },
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header", padding = 3, position = "relative" },
+				{ section = "keys", gap = 2, padding = 3, position = "relative" },
+				{ section = "startup" },
+				{
+					section = "terminal",
+					cmd = "ascii-image-converter ~/.config/nvim/yuji-min.jpg -C -c",
+					random = 10,
+					pane = 2,
+					indent = 3,
+					height = 40,
+					padding = 4,
+					position = "relative",
+				},
+			},
+		},
+		lazygit = { configure = false },
+
+		notifier = {
+			border = "rounded",
+			zindex = 100,
+			ft = "markdown",
+			wo = {
+				winblend = 5,
+				wrap = false,
+				conceallevel = 2,
+				colorcolumn = "",
+			},
+			bo = { filetype = "snacks_notif" },
+		},
+	},
 }
