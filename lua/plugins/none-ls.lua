84a5e4d7fd389e9f4e09bdc2fca10ab42ad83e9f
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-02 16:25:59 +0530

    Made linters invocation on write

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,21 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.pylint,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end,
 }
-
-
