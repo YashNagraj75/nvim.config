1afeb4a95093f773be61aa3614b5f933e348b9c5
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-14 12:38:53 +0530

    Add yazi as the file explorer

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,38 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("yazi").setup()
+	end,
+	keys = {
+		{
+			"<leader>-",
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>cw",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			-- NOTE: this requires a version of yazi that includes
+			-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+			"<c-up>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	---@type YaziConfig
+	opts = {
+		-- if you want to open yazi instead of netrw, see below for more info
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+			open_file_in_vertical_split = "<c-v>",
+			open_file_in_horizontal_split = "<leader>ch",
+			open_file_in_tab = "<c-n>",
+		},
+	},
+}
