09c50625b924cb2f8d90b3511828a5f190513793
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-01-21 10:40:03 +0530

    Add system clipboard as default clipboard

--- lua/vim-opts.lua
+++ lua/vim-opts.lua
@@ -2,9 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 vim.wo.relativenumber = true
-vim.keymap.set('n', '<leader>sh', ':split<CR>:enew<CR>', { noremap = true, silent = true })
-
-
-
+vim.keymap.set("n", "<leader>sh", ":split<CR>:enew<CR>", { noremap = true, silent = true })
+vim.cmd("set clipboard=unnamedplus")
