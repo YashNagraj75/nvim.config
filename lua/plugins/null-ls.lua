e571e8ed03075e374017d80fb5d45a84a10af96e
Yash Nagraj <pes1202203845@pesu.pes.edu>
2025-02-03 09:40:05 +0530

    Add formatters for go

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,23 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.pylint,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.biome,
-        null_ls.builtins.formatting.eslint_lsp,
-      },
-    })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function()
-        vim.lsp.buf.format({ async = false })
-      end,
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.pylint,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.biome,
+				null_ls.builtins.formatting.eslint_lsp,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.gofmt,
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end,
 }
