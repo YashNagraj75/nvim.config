de4659568b51bd3e0051221854a5eada3ca0a13c
YashNagraj75 <yashnagraj75@gmail.com>
2025-02-10 17:35:45 +0530

    Remove image.nvim as build was failing

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,7 +5,6 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim",
 	},
 	config = function()
 		vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>", {})
