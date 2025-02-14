6163546080e466fc6bf327e66e94ba332967ceee
YashNagraj75 <yashnagraj75@gmail.com>
2025-02-14 22:44:20 +0530

    Remove oil keymap

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,31 +1,31 @@
 return {
-	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("yazi").setup()
-	end,
-	keys = {
-		{
-			"<leader>-",
-			"<cmd>Yazi<cr>",
-			desc = "Open yazi at the current file",
-		},
-		{
-			"<leader>cw",
-			"<cmd>Yazi cwd<cr>",
-			desc = "Open the file manager in nvim's working directory",
-		},
-		{
-			"<c-up>",
-			"<cmd>Yazi toggle<cr>",
-			desc = "Resume the last yazi session",
-		},
-	},
-	---@type YaziConfig
-	opts = {
-		open_for_directories = false,
-		keymaps = {
-			show_help = "<f1>",
-		},
-	},
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("yazi").setup()
+  end,
+  keys = {
+    {
+      "<leader>-",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
 }
