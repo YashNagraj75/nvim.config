37a489d325760159c0fb62ef71b71eabe1dc4c57
YashNagraj75 <yashnagraj75@gmail.com>
2025-02-14 22:45:19 +0530

    Deleted oil setup

--- lua/plugins/oil.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
-	config = function()
-		vim.keymap.set("n", "o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
-}
