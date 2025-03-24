10cb9e6427bde7257b0d4b14c27bb843bd4b7f63
YashNagraj75 <yashnagraj75@gmail.com>
2025-03-24 10:00:09 +0530

    Add debuging for python

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -4,10 +4,21 @@ return {
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 	},
 	{
+		"leoluz/nvim-dap-go",
+	},
+	{
 		"mfussenegger/nvim-dap",
-		dependancies = { "rcarriga/nvim-dap-ui" },
+		dependancies = {
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+			"mfussenegger/nvim-dap-python",
+		},
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("dap-python").setup("/usr/bin/python")
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
 			end
